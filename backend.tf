4fcf6630681edbf73bf08d6afc56b7a3626b0487
Adrian Jimenez <ajimenezh.94@gmail.com>
2023-02-09 17:40:38 +0100

    Init terraform

---
+++ backend.tf
@@ -0,0 +1,8 @@
+terraform {
+    backend "s3" {
+        bucket = "euw1-bluetab-general-tfstate-pro"
+        key = "kinesis-flink-hudi.tfstate"
+        region = "eu-west-1"
+        profile = "practica"
+    }
+}
