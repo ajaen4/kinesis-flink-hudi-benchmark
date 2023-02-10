b46f96835ae56616da24a9bb956af764a622e5c3
alfonjerezi <alfonso.jerez@olympicchannel.com>
2023-02-10 17:06:26 +0100

    kinesis resources and sdk template

--- provider.tf
+++ provider.tf
@@ -1,4 +1,7 @@
 provider "aws" {
-  profile = "practica"
+  profile = "practica-hudi-flink"
   region     = var.AWS_REGION
+  #assume_role {
+  #  role_arn    = "arn:aws:iam::482861842012:role/practicahudiflink"
+  #}
 }
