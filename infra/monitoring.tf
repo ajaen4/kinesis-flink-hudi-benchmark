d7844f2a37b5429f46feba21686f09aba55a19f0
alfonjerezi <alfonso.jerez@olympicchannel.com>
2023-06-07 15:09:27 +0200

    including specific hudi output_format value

--- infra/monitoring.tf
+++ infra/monitoring.tf
@@ -5,7 +5,7 @@ module "monitoring_hudi_mor" {
   aws_region_name = var.aws_region
   aws_account_id  = data.aws_caller_identity.current.account_id
   image_tag       = "0.0.1"
-  output_format   = "hudi"
+  output_format   = "hudi_mor"
   module_name     = "hudi"
   database_name   = var.glue_database_name
   table_name      = var.hudi_mor_table_name
@@ -18,7 +18,7 @@ module "monitoring_hudi_cow" {
   aws_region_name = var.aws_region
   aws_account_id  = data.aws_caller_identity.current.account_id
   image_tag       = "0.0.1"
-  output_format   = "hudi"
+  output_format   = "hudi_cow"
   module_name     = "hudi"
   database_name   = var.glue_database_name
   table_name      = var.hudi_cow_table_name
