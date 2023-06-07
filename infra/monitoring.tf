208202876202f856db7e386dbdb44cf69a2c3063
alfonjerezi <alfonso.jerez@olympicchannel.com>
2023-06-07 18:26:28 +0200

    clean up

--- infra/monitoring.tf
+++ infra/monitoring.tf
@@ -5,8 +5,7 @@ module "monitoring_hudi_mor" {
   aws_region_name = var.aws_region
   aws_account_id  = data.aws_caller_identity.current.account_id
   image_tag       = "0.0.1"
-  output_format   = "hudi_mor"
-  module_name     = "hudi"
+  output_format     = "hudi-mor"
   database_name   = var.glue_database_name
   table_name      = var.hudi_mor_table_name
 }
@@ -18,8 +17,7 @@ module "monitoring_hudi_cow" {
   aws_region_name = var.aws_region
   aws_account_id  = data.aws_caller_identity.current.account_id
   image_tag       = "0.0.1"
-  output_format   = "hudi_cow"
-  module_name     = "hudi"
+  output_format     = "hudi-cow"
   database_name   = var.glue_database_name
   table_name      = var.hudi_cow_table_name
 }
@@ -32,7 +30,6 @@ module "monitoring_json" {
   aws_account_id  = data.aws_caller_identity.current.account_id
   image_tag       = "0.0.1"
   output_format   = "json"
-  module_name     = "json"
   database_name   = var.glue_database_name
   table_name      = var.json_table_name
 }
--- infra/vars/flink-hudi.tfvars
+++ infra/vars/flink-hudi.tfvars
@@ -2,8 +2,8 @@ aws_region            = "eu-west-1"
 artifacts_bucket_name = "flink-hudi-practica"
 source_stream_name    = "inbound_kinesis"
 glue_database_name    = "hudi"
-json_table_name       = "json_table"
-hudi_cow_table_name   = "ticker_hudi_cow_ro"
+json_table_name       = "table_json"
+hudi_cow_table_name   = "ticker_hudi_cow"
 hudi_mor_table_name   = "ticker_hudi_mor_ro"
 
 eks_tags = {
